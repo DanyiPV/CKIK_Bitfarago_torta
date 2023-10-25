3d154843f995e2e704d584f55559b44f3fc7e127
oroszadam2005 <94828122+oroszadam2005@users.noreply.github.com>
2023-10-25 09:01:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById("myCanvas");var ctx = canvas.getContext("2d");
 var canvasg = document.getElementById("CanvasGame");var ctxg = canvasg.getContext("2d");
+var canvasg2 = document.getElementById("CanvasGame2");var ctxg2 = canvasg2.getContext("2d");
 var Diff = "Könnyű"
 var mode = 5;
 function DiffChange(input,v){
@@ -16,6 +17,7 @@ function Inditas(){
     console.log(Diff,mode);
     canvas.style.opacity = 1;
     canvasg.style.opacity = 1;
+    canvasg2.style.opacity = 1;
     generate();
 }
 
@@ -35,13 +37,15 @@ Main();
 var r = Math.floor(Math.random() * 2);var positions = [];var placed = [];
 function generate(){
     if(r == 0){negyzet(mode)
-    }else{canvas.className = "circle";canvasg.className = "circle";kor(mode);}
+    }else{canvas.className = "circle";canvasg.className = "circle";canvasg2.className = "circle";kor(mode);}
 }
 function load(){
     setTimeout(function(){    
         if(r == 0){ctxg.beginPath();ctxg.rect(0, 0, 600, 600);ctxg.stroke();
-        }else{ctxg.beginPath();var center_x = 300;var center_y = 300;var radius = 300;ctxg.arc(center_x, center_y, radius, 0, 2 * Math.PI);ctxg.stroke();
-    }},1000)
+        }else{ctxg.beginPath();var center_x = 300;var center_y = 300;var radius = 300;ctxg.arc(center_x, center_y, radius, 0, 2 * Math.PI);ctxg.stroke();}
+        if(r == 0){ctxg2.beginPath();ctxg2.rect(0, 0, 600, 600);ctxg2.stroke();
+        }else{ctxg2.beginPath();var center_x = 300;var center_y = 300;var radius = 300;ctxg2.arc(center_x, center_y, radius, 0, 2 * Math.PI);ctxg2.stroke();}   
+    },1000)
 
     canvasg.addEventListener('click', event =>
     {
@@ -53,8 +57,27 @@ function load(){
             placed.push([x-1,y-1,inhand]);
             document.getElementById("tmp").id = "placed";
             inhand = -1;
+            if(placed.length == positions.length){
+                Ertekeles();
+            }
         }
     }); 
+}
+var kulonbseg = 0;
+function Ertekeles(){
+    for (let index = 0; index < positions.length; index++) {        
+        var asd = [];
+        var tmp = placed.filter((x) => x[2] == positions[index][2]);
+        var tmp2 = positions.filter((x) => x[2] == positions[index][2]);
+        for (let i = 0; i < tmp.length; i++) {
+            for (let i2 = 0; i2 < tmp2.length; i2++) {
+                asd.push([i,i2,Math.sqrt((Math.pow(tmp[i][0]-tmp2[i2][0],2)+Math.pow(tmp[i][1]-tmp2[i2][1],2)))]);
+            }
+        }
+
+        asd.map
+        console.log(asd); 
+    }
 }
 function negyzet(nehezseg){
     ctx.beginPath();ctx.rect(0, 0, 600, 600);ctx.stroke();
@@ -86,6 +109,7 @@ function Valaszt(png){
     tmp.id = "tmp";
     tmp.style.top ="-100px";
     inhand = "ph/"+ png +".png";
+    tmp.style.zIndex ="-3";
     document.onmousemove = update;
     function update( e )
     {
